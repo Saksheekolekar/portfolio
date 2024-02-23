f86079630a12b0fb95f748cbc09dad6d2eefdf1d
sakshi <sakshikolekar2003@gmail.com>
2024-02-24 00:49:34 +0530

    we made an home page with an little  animation on it.

---
+++ script.js
@@ -0,0 +1,28 @@
+// let nb=document.createElement("button");
+// nb.innerText="click me";
+
+// nb.style.backgroundColor="red";
+// document.querySelector("body").append(nb);
+
+// let p1=document.querySelector("p");
+//  p1.classList.add("para");
+
+
+//  //changing background color using event handling
+// let currentmode="light";
+//  nb.addEventListener("click",()=>{
+//     if(currentmode==="light")
+//     {
+//             currentmode="dark";
+//     document.querySelector("body").style.backgroundColor="white";
+//     nb.innerText="dark mode";
+//     }
+//     else
+//     {
+//         currentmode="light";
+//         document.querySelector("body").style.backgroundColor="black";
+//         nb.innerText="light mode";
+//     }
+//  });
+
+ let body=document.querySelector("body")
